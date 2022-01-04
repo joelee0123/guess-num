f8570daaa03eecc59d9cd19712422e8143dfe5de
JOELEE0123 <joelee650123@gmail.com>
2022-01-04 14:54:13 +0800

    debug if error

--- guess.py
+++ guess.py
@@ -14,10 +14,8 @@ while True:
 		print('你猜中了')
 		print('這是你猜了第', count, '次')
 		break
-	elif:
-		num > r
+	elif num > r:
 		print('比答案大')
-	elif:
-		num < r
+	elif num < r:
 		print('比答案小')
 	print('這是你猜了第', count, '次')
